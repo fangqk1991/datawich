cd27b0be24085038b6f39a9f94ad0d1fa09971cf
Fang <me@fangqk.com>
2023-04-16 13:44:39 +0800

    (datawich-react): adjust dialog theme

--- packages/datawich-react/src/app/App.tsx
+++ packages/datawich-react/src/app/App.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { ErrorBoundary } from '@ant-design/pro-components'
 import { RouterProvider } from 'react-router-dom'
 import { MyRouter } from './MyRouter'
+import { ReactDialogTheme } from '@fangcha/react'
 import { VisitorProvider } from '@fangcha/auth-react'
+
+ReactDialogTheme.colorPrimary = 'rgb(221 115 164)'
 
 export const App: React.FC = () => {
   return (
