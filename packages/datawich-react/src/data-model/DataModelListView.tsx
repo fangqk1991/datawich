da1dbe3a03b514d7bc6ea6f9b3732d4910ea1fa9
Fang <me@fangqk.com>
2023-05-03 12:48:19 +0800

    (datawich-react): +DataModelListView

--- packages/datawich-react/src/data-model/DataModelListView.tsx
+++ packages/datawich-react/src/data-model/DataModelListView.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { MyRequest } from '@fangcha/auth-react'
-import { Breadcrumb, Card, Divider, Spin } from 'antd'
+import { Breadcrumb, Card, Divider, Space, Spin, Tag, Tooltip } from 'antd'
 import { DataAppApis } from '@web/datawich-common/web-api'
 import { DataModelModel } from '@fangcha/datawich-service/lib'
 import { useNavigate } from 'react-router-dom'
 import { LS } from '../core/ReactI18n'
+import { CheckCircleOutlined, InfoCircleOutlined, WarningOutlined } from '@ant-design/icons'
 
 export const DataModelListView: React.FC = () => {
   const [appList, setAppList] = useState<DataModelModel[]>()
@@ -37,13 +38,72 @@ export const DataModelListView: React.FC = () => {
           <Card.Grid
             style={{
               cursor: 'pointer',
+              padding: '16px',
             }}
             key={dataApp.modelKey}
             onClick={() => {
               navigate(`/v1/data-app/${dataApp.modelKey}`)
             }}
           >
-            <b>{dataApp.name}</b>
+            <h4
+              style={{
+                margin: '8px 0',
+                marginBlockStart: '0',
+                marginBlockEnd: '0',
+              }}
+            >
+              {dataApp.name}{' '}
+              <Tooltip
+                title={
+                  <ul
+                    style={{
+                      paddingInlineStart: '12px',
+                    }}
+                  >
+                    <li>modelKey: {dataApp.modelKey}</li>
+                    <li>维护者: {dataApp.author}</li>
+                  </ul>
+                }
+              >
+                <InfoCircleOutlined />
+              </Tooltip>
+            </h4>
+            <ul
+              style={{
+                paddingInlineStart: '12px',
+              }}
+            >
+              <li>
+                <Space>
+                  {dataApp.isOnline && (
+                    <span
+                      style={{
+                        color: '#67C23A',
+                      }}
+                    >
+                      已发布 <CheckCircleOutlined />
+                    </span>
+                  )}
+                  {!dataApp.isOnline && (
+                    <span
+                      style={{
+                        color: '#F56C6C',
+                      }}
+                    >
+                      未发布 <WarningOutlined />
+                    </span>
+                  )}
+                  {!!dataApp.isRetained && <Tag color={'error'}>系统保留</Tag>}
+                  {!!dataApp.isLibrary && <Tag color={'geekblue'}>可关联</Tag>}
+                  {!!dataApp.tagList &&
+                    dataApp.tagList.map((item) => (
+                      <Tag key={item} color={'error'}>
+                        {item}
+                      </Tag>
+                    ))}
+                </Space>
+              </li>
+            </ul>
           </Card.Grid>
         ))}
       </Card>
