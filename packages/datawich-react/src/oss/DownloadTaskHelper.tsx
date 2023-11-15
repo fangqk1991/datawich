9463d8f86c09b121d1bf4af1556973a976f247a2
Fang <me@fangqk.com>
2023-11-15 16:35:01 +0800

    (datawich-react): + DownloadTaskHelper use MessageDialog

--- packages/datawich-react/src/oss/DownloadTaskHelper.tsx
+++ packages/datawich-react/src/oss/DownloadTaskHelper.tsx
@@ -1,13 +1,14 @@
 import { ResourceTaskModel, ResourceTaskStatus } from '@fangcha/oss-models'
 import { DatawichPages } from '@web/datawich-common/admin-apis'
 import { message } from 'antd'
+import { MessageDialog } from '@fangcha/react'
 
 export class DownloadTaskHelper {
   public static handleDownloadResponse(response: ResourceTaskModel) {
     switch (response.taskStatus) {
       case ResourceTaskStatus.Pending:
       case ResourceTaskStatus.Processing: {
-        message.success(
+        MessageDialog.alert(
           <>
             任务已提交，生成完毕后可在{' '}
             <a href={DatawichPages.ResourceTaskListRoute} target='_blank'>
