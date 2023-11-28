ae9784fe6adbc03e0c29d5ac5948d5847f1998d7
Fang <me@fangqk.com>
2023-11-28 17:39:53 +0800

    (datawich-react): + DataModel import

--- packages/datawich-react/src/data-model/DataModelListView.tsx
+++ packages/datawich-react/src/data-model/DataModelListView.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { MyRequest } from '@fangcha/auth-react'
-import { Breadcrumb, Card, Divider, Spin } from 'antd'
-import { DataAppApis } from '@web/datawich-common/web-api'
+import { Breadcrumb, Button, Card, Divider, message, Space, Spin } from 'antd'
+import { DataAppApis, DataModelApis } from '@web/datawich-common/web-api'
 import { DataModelModel } from '@fangcha/datawich-service'
 import { LS } from '../core/ReactI18n'
 import { DataModelCard } from './DataModelCard'
+import { JsonEditorDialog } from '@fangcha/react'
+import { DataModelDialog } from './DataModelDialog'
 
 export const DataModelListView: React.FC = () => {
   const [appList, setAppList] = useState<DataModelModel[]>()
+  const [version, setVersion] = useState(0)
 
   useEffect(() => {
     MyRequest(DataAppApis.DataAppListGet)
@@ -15,7 +18,7 @@ export const DataModelListView: React.FC = () => {
       .then((response) => {
         setAppList(response)
       })
-  }, [])
+  }, [version])
 
   if (!appList) {
     return <Spin size='large' />
@@ -30,7 +33,43 @@ export const DataModelListView: React.FC = () => {
           },
         ]}
       />
-      <Divider style={{ margin: '12px 0' }} />
+      <Divider />
+      <Space>
+        <Button
+          type='primary'
+          onClick={() => {
+            const dialog = new DataModelDialog({
+              title: '创建模型',
+            })
+            dialog.show(async (params) => {
+              const request = MyRequest(DataModelApis.DataModelCreate)
+              request.setBodyData(params)
+              await request.quickSend()
+              message.success('创建成功')
+              setVersion(version + 1)
+            })
+          }}
+        >
+          创建
+        </Button>
+        <Button
+          onClick={() => {
+            const dialog = new JsonEditorDialog({
+              title: '导入模型 JSON',
+            })
+            dialog.show(async (params) => {
+              const request = MyRequest(DataModelApis.DataModelImport)
+              request.setBodyData(params)
+              await request.quickSend()
+              message.success('导入成功')
+              setVersion(version + 1)
+            })
+          }}
+        >
+          导入
+        </Button>
+      </Space>
+      <Divider />
       <Card>
         {appList.map((dataApp) => (
           <Card.Grid
