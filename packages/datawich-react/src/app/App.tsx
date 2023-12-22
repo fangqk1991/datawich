514361da3f6f06056357a05c39451d603f54b859
Fang <me@fangqk.com>
2023-12-22 16:11:56 +0800

    (datawich-react): update defaultRedirectUri

--- packages/datawich-react/src/app/App.tsx
+++ packages/datawich-react/src/app/App.tsx
@@ -3,12 +3,13 @@ import { ErrorBoundary } from '@ant-design/pro-components'
 import { RouterProvider } from 'react-router-dom'
 import { MyRouter } from './MyRouter'
 import { ReactTheme } from '@fangcha/react'
-import { useSessionConfig } from '@fangcha/auth-react'
+import { AuthSdkHelper, useSessionConfig } from '@fangcha/auth-react'
+import { OssSDK } from '@fangcha/oss-react'
 import './app.scss'
 import './bootstrap-like.scss'
-import { OssSDK } from '@fangcha/oss-react'
 
 ReactTheme.colorPrimary = 'rgb(221 115 164)'
+AuthSdkHelper.defaultRedirectUri = '/'
 
 export const App: React.FC = () => {
   const config = useSessionConfig() as any
