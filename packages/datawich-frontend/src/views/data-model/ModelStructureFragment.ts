d1602b5878e624aebba13b2ea51c856414e2301b
Fang <me@fangqk.com>
2024-01-17 15:44:14 +0800

    (datawich-frontend): remove FieldLinkTable

--- packages/datawich-frontend/src/views/data-model/ModelStructureFragment.ts
+++ packages/datawich-frontend/src/views/data-model/ModelStructureFragment.ts
@@ -1,20 +1,15 @@
 import { Component } from '@fangcha/vue'
 import { ModelFieldTable } from '../model-field/ModelFieldTable'
-import { FieldLinkTable } from '../model-field/FieldLinkTable'
 import { ModelFragmentBase } from './ModelFragmentBase'
 
 @Component({
   components: {
     'model-field-table': ModelFieldTable,
-    'field-link-table': FieldLinkTable,
   },
   template: `
     <div v-if="dataModel">
       <el-card shadow="never">
         <model-field-table @loadWidgetsInfo="onLoadWidgetsInfo" ref="fieldTableView" :model-key="modelKey" />
-      </el-card>
-      <el-card class="mt-4" shadow="never">
-        <field-link-table ref="linkTableView" :model-key="modelKey" />
       </el-card>
     </div>
   `,
@@ -24,10 +19,6 @@ export class ModelStructureFragment extends ModelFragmentBase {
     this.$nextTick(() => {
       {
         const tableView = this.$refs.fieldTableView as ModelFieldTable
-        tableView.reloadData()
-      }
-      {
-        const tableView = this.$refs.linkTableView as FieldLinkTable
         tableView.reloadData()
       }
     })
