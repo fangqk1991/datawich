89e3b2a75e93796b567983963b92f56578ccbab2
Fang <me@fangqk.com>
2024-01-18 13:56:24 +0800

    (web-frontend): use allowAnonymous

--- packages/web-frontend/src/index.tsx
+++ packages/web-frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { App } from './app/App'
 const app = ReactDOM.createRoot(document.getElementById('app')!)
 app.render(
   <React.StrictMode>
-    <LaunchContainer>
+    <LaunchContainer allowAnonymous={true}>
       <App />
     </LaunchContainer>
   </React.StrictMode>
