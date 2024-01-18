1fecd75aaa2cd5159e5959ae2f3c9af3eb76f828
Fang <me@fangqk.com>
2024-01-18 17:13:51 +0800

    (datawich-backend): pm2 + datawich-web

--- packages/datawich-backend/ecosystem.config.js
+++ packages/datawich-backend/ecosystem.config.js
@@ -50,6 +50,28 @@ const appList = [
       NODE_CONFIG_ENV: 'production',
     },
   },
+  {
+    name: 'datawich-web',
+    script: `${rootDir}/packages/datawich-backend/dist/datawich-web.js`,
+    error_file: `${logsDir}/datawich-web-err.log`,
+    out_file: `${logsDir}/datawich-web-out.log`,
+    exec_mode: 'fork',
+    listen_timeout: 10000,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
+    env: {
+      CODE_VERSION: 'COMMIT_SHA',
+      NODE_ENV: 'development',
+      NODE_CONFIG_ENV: 'development',
+    },
+    env_staging: {
+      NODE_ENV: 'staging',
+      NODE_CONFIG_ENV: 'staging',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      NODE_CONFIG_ENV: 'production',
+    },
+  },
 ]
 
 const resqueApp = {
