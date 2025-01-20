547873ca130f8239d5cf710b9872b111d7630e59
Fang <me@fangqk.com>
2025-01-21 00:38:38 +0800

    (datawich-react): + DataExcelHelper, ExcelExportHandler

---
+++ libraries/datawich-react/excel/ExcelExportHandler.tsx
@@ -0,0 +1,32 @@
+import { FormField } from '@fangcha/form-models'
+import { DatawichExcelHelper } from '@fangcha/datawich-service/lib/excel'
+const { saveAs } = require('file-saver')
+
+interface Props {
+  name: string
+  fieldItems: FormField[]
+}
+
+export class ExcelExportHandler {
+  public readonly table: Props
+
+  constructor(table: Props) {
+    this.table = table
+  }
+
+  public async exportExcel(fileName: string, rowList: any[]) {
+    const buffer = await DatawichExcelHelper.makeExcelBuffer(this.table.fieldItems, rowList)
+    const blob = new Blob([buffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    })
+    saveAs(blob, `${fileName}.xlsx`)
+  }
+
+  public async exportTemplateFile() {
+    return this.exportExcel(`${this.table.name}-模板`, [DatawichExcelHelper.getTableExampleData(this.table.fieldItems)])
+  }
+
+  public async exportRecords(records: any[]) {
+    return this.exportExcel(`${this.table.name}-数据`, records)
+  }
+}
---
+++ libraries/datawich-service/src/excel/DatawichExcelHelper.ts
@@ -0,0 +1,103 @@
+import * as moment from 'moment/moment'
+import assert from '@fangcha/assert'
+import { TypicalColumn, TypicalExcel } from '@fangcha/excel'
+import { FT } from '@fangcha/tools'
+import { FormField, FormFieldType } from '@fangcha/form-models'
+
+export class DatawichExcelHelper {
+  public static getFieldExampleValue(fieldType: FormFieldType) {
+    switch (fieldType) {
+      case FormFieldType.String:
+        return 'Some text'
+      case FormFieldType.Number:
+        return 0
+      case FormFieldType.Date:
+        return '2023-08-01'
+      case FormFieldType.Datetime:
+        return '2023-08-01 00:00:00'
+    }
+    return 'Some text'
+  }
+
+  public static getTableExampleData(fieldItems: FormField[]) {
+    return fieldItems.reduce((result, field) => {
+      result[field.fieldKey] = this.getFieldExampleValue(field.fieldType)
+      return result
+    }, {})
+  }
+
+  public static transferExcelRecords(fieldItems: FormField[], records: any[]) {
+    return records.map((rawData) => {
+      const data: any = {}
+      if (rawData.data_id) {
+        data.data_id = rawData.data_id
+      }
+      fieldItems.forEach((field) => {
+        data[field.fieldKey] = rawData[field.fieldKey] !== undefined ? rawData[field.fieldKey] : null
+        if (field.fieldType === FormFieldType.Datetime && data[field.fieldKey]) {
+          const time = moment(data[field.fieldKey])
+          data[field.fieldKey] = time.isValid() ? time.format() : null
+        }
+      })
+      return data
+    })
+  }
+
+  public static purifyData(fieldItems: FormField[], data: any) {
+    data = fieldItems
+      .filter((field) => data[field.fieldKey] !== undefined)
+      .reduce((result, field) => {
+        result[field.fieldKey] = data[field.fieldKey]
+        return result
+      }, {})
+    data = { ...data }
+    fieldItems
+      .filter((field) => field.fieldType !== FormFieldType.String)
+      .forEach((field) => {
+        if (data[field.fieldKey] === '') {
+          data[field.fieldKey] = null
+        }
+      })
+    for (const field of fieldItems) {
+      const value = data[field.fieldKey]
+      if (!value) {
+        continue
+      }
+      switch (field.fieldType) {
+        case FormFieldType.Number:
+          assert.ok(!isNaN(value), `${field.name} 值格式有误`)
+          break
+        case FormFieldType.Date:
+        case FormFieldType.Datetime:
+          assert.ok(moment(value).isValid(), `${field.name} 值格式有误`)
+          break
+      }
+    }
+    return data
+  }
+
+  public static async makeExcelBuffer<T extends object = any>(fieldItems: FormField[], rowList: T[]) {
+    const columns: TypicalColumn<T>[] = [
+      ...fieldItems.map((field) => {
+        if (field.fieldType === FormFieldType.Datetime) {
+          return {
+            columnKey: field.fieldKey,
+            columnName: field.name,
+            columnValue: (item: any) => {
+              return item[field.fieldKey] ? FT(item[field.fieldKey]) : ''
+            },
+            width: 18,
+          }
+        }
+        return {
+          columnKey: field.fieldKey,
+          columnName: field.name,
+          width: 10,
+        }
+      }),
+    ]
+    const excel = TypicalExcel.excelWithTypicalColumns(columns)
+    excel.addTypicalRowList(rowList)
+    return excel.writeBuffer()
+  }
+}
